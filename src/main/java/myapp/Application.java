4bf2cd9bc4d3819d3c5dc278ad12ead7fa7c61b1
Duong <e1500932@edu.vamk.fi>
2018-04-18 14:03:34 +0300

    Heroku support added

--- src/main/java/myapp/Application.java
+++ src/main/java/myapp/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Now supports Travis
+//Now also Heroku activated
 @SpringBootApplication
  //@Import (SwaggerConfig.class) //for Swagger documentation - Swagger home
 // http://localhost:8080/swagger-ui.html
